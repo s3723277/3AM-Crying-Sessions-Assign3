f600cb5ec68f72cadcd14eaedb70a260c4de5d6b
s3720825 <62202231+s3720825@users.noreply.github.com>
2020-10-07 19:27:23 +1100

    Delete foot.php

--- a3/includes/foot.php
+++
@@ -1,6 +0,0 @@
-<div class="card" style="background-color:#FF5757;">
-  <div class="card-body">
-    <h5 class="card-title">Boomer Life Support</h5>
-    <p class="card-text">Footer</p>
-  </div>
-</div>
